fd8c411522fa05cf2812008881652b3fa422fc49
Vale <valentinahdz0296@gmail.com>
2024-08-28 17:41:05 -0600

    se aplica el uso de query parameters

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-const URL='https://api.thecatapi.com/v1/images/search?';
+const URL='https://api.thecatapi.com/v1/images/search?limit=3';
 
 async function LoadCatImage(){
     try{
-        const rest = await fetch (URL);
-        const data = await rest.json();
-        const img = document.querySelector('img');
-        img.src = data [0].url;
+        const res = await fetch (URL);
+        const data = await res.json();
+
+        console.log (data)
+        const img1 = document.getElementById('img1');
+        const img2 = document.getElementById('img2');
+        const img3 = document.getElementById('img3');
+        
+        img1.src = data [0].url;
+        img2.src = data [1].url;
+        img3.src = data [2].url;
+
         document.getElementById('reloadButton').onclick= function(){
         location.reload();
         }
-    }catch (error){
+    } catch (error){
         console.log('Error al obtener la imagen del gato:', error);
     }
 }
-
-
 
 LoadCatImage();
